dfc452ba92df7b6c7f8820a42d1bfd95c78888cf
George-Dev-Web <georgemwazuna3@gmail.com>
2025-02-03 16:15:37 +0300

    Added the menu part for the various dishes

---
+++ script/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let dropdown = document.querySelector(".dropdown");
+
+  dropdown.addEventListener("mouseenter", function () {
+    this.querySelector(".dropdown-content").style.display = "block";
+  });
+
+  dropdown.addEventListener("mouseleave", function () {
+    this.querySelector(".dropdown-content").style.display = "none";
+  });
+});
